87518ae1b86251b0f60fad54d9638704677afbe0
Theo Fabi <fabi.theo@gmail.com>
2026-09-07 11:28:12 -0400

    wip: ElfContext integration into example

--- decoder/examples/example.zig
+++ decoder/examples/example.zig
@@ -58,54 +58,7 @@ const Context = struct {
     io: Io,
     allocator: std.mem.Allocator,
     terminal: Io.Terminal,
-    dump: Objdump = .empty,
-    mapped_mem: []align(std.heap.page_size_min) const u8 = &.{},
-    regions: []const opencsd.file_mem_region_t = &.{},
-    csh: capstone.csh = undefined,
-
-    const AddressRangeQuery = struct {
-        range: [2]u64,
-
-        pub fn init(range: [2]u64) AddressRangeQuery {
-            return .{ .range = range };
-        }
-    };
-
-    fn findRegionIndex(ctx: *const Context, query: AddressRangeQuery) ?usize {
-        return for (0.., ctx.regions) |i, r| {
-            if (r.start_address <= query.range[0] and query.range[1] <= r.start_address + r.region_size) {
-                break i;
-            }
-        } else null;
-    }
-
-    fn findDumpChunk(ctx: *const Context, query: AddressRangeQuery) ?*const Objdump.Chunk {
-        const region_index = ctx.findRegionIndex(query) orelse return null;
-        const chunk = &ctx.dump.chunks[region_index];
-        std.debug.assert(chunk.start <= query.range[0] and query.range[1] <= chunk.stop);
-        return chunk;
-    }
-
-    fn getCode(ctx: *const Context, query: AddressRangeQuery) ?[]const u8 {
-        const region_index = ctx.findRegionIndex(query) orelse return null;
-        const region = ctx.regions[region_index];
-        const query_size = query.range[1] - query.range[0];
-        const offs_from_start = query.range[0] - region.start_address;
-        return ctx.mapped_mem[region.file_offset + offs_from_start..][0..query_size];
-    }
-
-    fn disasm(ctx: *const Context, code: []const u8, query: AddressRangeQuery) ![]capstone.cs_insn {
-        var insn: ?[*]capstone.cs_insn = null;
-        const count = capstone.cs_disasm(ctx.csh, code.ptr, code.len, query.range[0], 0, &insn);
-        if (count == 0) {
-            std.log.err("capstone error during disasm: {s}", .{
-                @as([*:0]const u8, @ptrCast(capstone.cs_strerror(capstone.cs_errno(ctx.csh)))),
-            });
-            return error.CapstoneError;
-        }
-        const instr_list = (insn.?)[0..count];
-        return instr_list;
-    }
+    elf: *opencsd.ElfContext,
 };
 
 fn loggerPrint(
@@ -117,33 +70,6 @@ fn loggerPrint(
     if (strp == null) return;
     const str: [:0]const u8 = if (str_len > 0) strp[0..@intCast(str_len):0] else std.mem.span(strp);
     ctx.terminal.writer.writeAll(str) catch {};
-}
-
-fn fillRegions(
-    file: Io.File,
-    io: Io,
-    gpa: std.mem.Allocator,
-    regions: *std.ArrayList(opencsd.file_mem_region_t),
-) !void {
-    var buffer: [512]u8 = undefined;
-    var elf_file_reader = file.reader(io, &buffer);
-    const header = try std.elf.Header.read(&elf_file_reader.interface);
-    var it = header.iterateProgramHeaders(&elf_file_reader);
-    while (try it.next()) |phdr_raw| {
-        const phdr: std.elf.Elf64.Phdr = @bitCast(phdr_raw);
-        if (phdr.flags.X and phdr.type == .LOAD and phdr.memsz > 0 and phdr.memsz == phdr.filesz) {
-            try regions.append(gpa, .{
-                .file_offset = @intCast(phdr.offset),
-                .region_size = @intCast(phdr.memsz),
-                .start_address = @intCast(phdr.vaddr),
-            });
-        }
-    }
-    std.mem.sort(opencsd.file_mem_region_t, regions.items, {}, struct {
-        fn call(_: void, lhs: opencsd.file_mem_region_t, rhs: opencsd.file_mem_region_t) bool {
-            return lhs.start_address < rhs.start_address;
-        }
-    }.call);
 }
 
 fn printTraceElemInner(
@@ -179,28 +105,9 @@ fn printTraceElemInner(
                 },
                 else => {},
             }
-            const query: Context.AddressRangeQuery = .init(.{elem.start_address, elem.end_address});
-            const chunk = ctx.findDumpChunk(query) orelse break :x;
-            var addr_index: usize = for (0.., chunk.addrs) |i, offs| {
-                const addr: u64 = chunk.start + @as(u64, offs);
-                if (addr == elem.start_address) break i;
-                if (addr > elem.start_address) break :x;
-            } else break :x;
-
-            while (addr_index < chunk.addrs.len) : (addr_index += 1) {
-                const curr_addr = chunk.start + @as(u64, chunk.addrs[addr_index]);
-                if (curr_addr >= elem.end_address) break;
-                std.debug.assert(curr_addr >= elem.start_address);
-                const strp = chunk.strps[addr_index];
-                const line = std.mem.sliceTo(chunk.output[strp..], '\n');
-                try writer.print("0x{X:08}:\t{s}\n", .{ curr_addr, line });
-            }
-            try writer.writeByte('\n');
-
-            const code = ctx.getCode(query).?;
-            const instr_list = ctx.disasm(code, query) catch break :x;
+            const query: opencsd.ElfContext.AddressRangeQuery = .init(.{elem.start_address, elem.end_address});
+            const instr_list = (ctx.elf.disassembleAddressRange(query) catch null) orelse break :x;
             defer _ = capstone.cs_free(instr_list.ptr, instr_list.len);
-
             var max_width: usize = 0;
             for (instr_list) |*instr| max_width = @max(max_width, std.mem.sliceTo(&instr.mnemonic, 0).len);
 
@@ -242,168 +149,6 @@ fn printTraceElem(
         error.Unexpected, error.Canceled => return opencsd.RESP_FATAL_SYS_ERR,
     };
     return opencsd.RESP_CONT;
-}
-
-const ObjdumpOptions = struct {
-    exe: []const u8 = "objdump",
-    target_query: ?TargetQuery = null,
-};
-
-const Objdump = struct {
-    chunks: []const Chunk,
-
-    const empty: Objdump = .{ .chunks = &.{} };
-
-    fn deinit(objdump: *const Objdump, gpa: std.mem.Allocator) void {
-        for (objdump.chunks) |chunk| chunk.deinit(gpa);
-        gpa.free(objdump.chunks);
-    }
-
-    const Chunk = struct {
-        output: []const u8,
-        start: u64,
-        stop: u64,
-        /// A sorted list of offsets starting from `start`, i.e. a value of 6 here corresponds to the address `start+6`
-        addrs: []const u32,
-        /// The offset of the disassembled instruction string within `output`
-        strps: []const u32,
-
-        fn deinit(chunk: *const Chunk, gpa: std.mem.Allocator) void {
-            gpa.free(chunk.output);
-            gpa.free(chunk.addrs);
-            gpa.free(chunk.strps);
-        }
-    };
-};
-
-fn dumpChunk(
-    elf_filepath: [:0]const u8,
-    io: Io,
-    gpa: std.mem.Allocator,
-    tmp_arena: std.mem.Allocator,
-    region: *const opencsd.file_mem_region_t,
-    aw: *Io.Writer.Allocating,
-    options: ObjdumpOptions,
-) !Objdump.Chunk {
-    _ = &gpa;
-    var argv: std.ArrayList([]const u8) = .empty;
-    try argv.appendSlice(tmp_arena, &.{
-        options.exe,
-        "--no-show-raw-insn",
-        "-d",
-        try std.fmt.allocPrint(tmp_arena, "--start-address={}", .{region.start_address}),
-        // TODO: is this argument end exclusive?
-        try std.fmt.allocPrint(tmp_arena, "--stop-address={}", .{region.start_address+region.region_size}),
-    });
-
-    if (options.target_query) |*tq| {
-        if (tq.cpu_model != null and tq.cpu_model.?.llvm_name != null) {
-            const arg = try std.fmt.allocPrint(tmp_arena, "--mcpu={s}", .{tq.cpu_model.?.llvm_name.?});
-            try argv.append(tmp_arena, arg);
-        }
-
-        if (!tq.cpu_features_add.isEmpty() or !tq.cpu_features_sub.isEmpty()) {
-            var attrs: std.ArrayList(u8) = .empty;
-            try tq.toLlvmAttrs(&attrs, tmp_arena);
-            if (attrs.items.len > 0) {
-                try attrs.insertSlice(tmp_arena, 0, "--mattr=");
-                try argv.append(tmp_arena, attrs.items);
-            }
-        }
-    }
-
-    try argv.append(tmp_arena, elf_filepath);
-
-    std.debug.assert(aw.writer.end == 0);
-    try aw.ensureTotalCapacity((region.region_size * (8 + 1 + 4)) / 4);
-
-    {
-        var child = try std.process.spawn(io, .{
-            .argv = argv.items,
-            .stdout = .pipe,
-        });
-        defer child.kill(io);
-
-        var stdout_buffer: [0x4000]u8 = undefined;
-        var stdout_file_reader = child.stdout.?.readerStreaming(io, &stdout_buffer);
-        _ = try stdout_file_reader.interface.streamRemaining(&aw.writer);
-
-        const term = try child.wait(io);
-        switch (term) {
-            .exited => |code| if (code != 0) return error.ChildProcessError,
-            else => return error.Unexpected,
-        }
-    }
-
-    var addrs: std.ArrayList(u32) = try .initCapacity(gpa, region.region_size / 8);
-    defer addrs.deinit(gpa);
-    var strps: std.ArrayList(u32) = try .initCapacity(gpa, region.region_size / 8);
-    defer strps.deinit(gpa);
-
-    var it = std.mem.splitScalar(u8, aw.written(), '\n');
-    const baseptr = aw.written().ptr;
-    while (it.next()) |line| {
-        if (line.len == 0) continue;
-        const colon = std.mem.findScalar(u8, line, ':') orelse continue;
-        const next_non_ws = std.mem.findNonePos(u8, line, colon+1, " \t") orelse continue;
-        if (next_non_ws == '<') {
-            continue;
-        }
-        const index = (line.ptr + next_non_ws) - baseptr;
-        const addr = std.fmt.parseInt(u64, line[0..colon], 16) catch |err| switch (err) {
-            error.InvalidCharacter => continue,
-            else => |e| return e,
-        };
-        const addr_offset: u32 = @intCast(addr - region.start_address);
-        try addrs.append(gpa, addr_offset);
-        try strps.append(gpa, @intCast(index));
-    }
-
-    const output = try gpa.dupe(u8, aw.written());
-    errdefer gpa.free(output);
-    const addrs_duped = try addrs.toOwnedSlice(gpa);
-    errdefer gpa.free(addrs_duped);
-    const strps_duped = try strps.toOwnedSlice(gpa);
-    errdefer gpa.free(strps_duped);
-    return .{
-        .output = output,
-        .start = region.start_address,
-        .stop = region.start_address + region.region_size,
-        .addrs = addrs_duped,
-        .strps = strps_duped,
-    };
-}
-
-fn collectObjdump(
-    elf_filepath: [:0]const u8,
-    io: Io,
-    gpa: std.mem.Allocator,
-    regions: []const opencsd.file_mem_region_t,
-    options: ObjdumpOptions,
-) !Objdump {
-    var tmp_arena_instance: std.heap.ArenaAllocator = .init(std.heap.page_allocator);
-    defer tmp_arena_instance.deinit();
-    const tmp_arena = tmp_arena_instance.allocator();
-
-    var aw: Io.Writer.Allocating = .init(gpa);
-    defer aw.deinit();
-
-    const chunks_buffer = try gpa.alloc(Objdump.Chunk, regions.len);
-    var chunks: std.ArrayList(Objdump.Chunk) = .initBuffer(chunks_buffer);
-
-    errdefer {
-        for (chunks.items) |chunk| chunk.deinit(gpa);
-        gpa.free(chunks_buffer);
-    }
-
-    for (regions) |region| {
-        _ = tmp_arena_instance.reset(.retain_capacity);
-        aw.clearRetainingCapacity();
-        const chunk = try dumpChunk(elf_filepath, io, gpa, tmp_arena, &region, &aw, options);
-        try chunks.appendBounded(chunk);
-    }
-
-    return .{ .chunks = chunks_buffer };
 }
 
 fn csTry(e: capstone.cs_err) error{CapstoneError}!void {
@@ -515,13 +260,21 @@ pub fn main(init: std.process.Init) !void {
         .writer = &stdout_file_writer.interface,
     };
 
+    var elf_context: opencsd.ElfContext = try .open(program_elf_path, io, init.gpa, .{});
+    defer elf_context.deinit(init.gpa);
+    const arch_info = elf_context.getArchVersionAndCoreProfile();
+
     var context: Context = .{
         .allocator = init.gpa,
         .io = init.io,
         .terminal = terminal,
+        .elf = &elf_context,
     };
 
     try opencsd.checkError(opencsd.def_errlog_set_strprint_cb(dt.handle, @ptrCast(&context), &loggerPrint));
+
+    try elf_context.setupMemoryAccessor(dt);
+    defer elf_context.removeMemoryAccessor(dt);
 
     const configr = std.mem.zeroInit(CONFIGR, .{
         .ts = true,
@@ -529,8 +282,8 @@ pub fn main(init: std.process.Init) !void {
         .cci = true,
     });
     const trace_config: opencsd.etmv4_cfg = .{
-        .arch_ver = opencsd.c.ARCH_V8,
-        .core_prof = opencsd.c.profile_CortexM,
+        .arch_ver = arch_info.arch_ver orelse opencsd.c.ARCH_V8,
+        .core_prof = arch_info.core_profile,
         .reg_traceidr = 1,
         .reg_idr0 = 0x280006E1,
         .reg_idr8 = 0,
@@ -553,91 +306,7 @@ pub fn main(init: std.process.Init) !void {
 
     try opencsd.checkError(opencsd.dt_set_gen_elem_outfn(dt.handle, &printTraceElem, &context));
 
-    var csh: capstone.csh = undefined;
-    // NOTE: *not* setting CS_MODE_MCLASS fails the decoding of some instructions
-    // TODO: should capstone.CS_MODE_V8 be set here, too? it doesn't *seem* to cause
-    //       problems but idk
-    const csmode: c_uint = capstone.CS_MODE_THUMB | capstone.CS_MODE_MCLASS;
-    if (capstone.cs_open(capstone.CS_ARCH_ARM, csmode, &csh) != capstone.CS_ERR_OK) {
-        return error.CapstoneOpen;
-    }
-    defer _ = capstone.cs_close(&csh);
-    context.csh = csh;
-
-    const elf_file = try Io.Dir.openFile(.cwd(), io, program_elf_path, .{
-        .mode = .read_only,
-        .allow_directory = false,
-    });
-    context.mapped_mem = mapped: {
-        const file_len = std.math.cast(
-            usize,
-            elf_file.length(io) catch |err| switch (err) {
-                error.PermissionDenied => unreachable, // not asking for PROT_EXEC
-                else => |e| return e,
-            },
-        ) orelse return error.Overflow;
-
-        break :mapped std.posix.mmap(
-            null,
-            file_len,
-            .{ .READ = true },
-            .{ .TYPE = .SHARED },
-            elf_file.handle,
-            0,
-        ) catch |err| switch (err) {
-            error.MappingAlreadyExists => unreachable, // not using FIXED_NOREPLACE
-            error.PermissionDenied => unreachable, // not asking for PROT_EXEC
-            else => |e| return e,
-        };
-    };
-    defer std.posix.munmap(context.mapped_mem);
-
-    var regions: std.ArrayList(opencsd.file_mem_region_t) = .empty;
-    {
-        defer elf_file.close(io);
-        try fillRegions(elf_file, io, arena, &regions);
-        for (regions.items) |region| {
-            std.debug.print("fileoff: 0x{X:08}, vmaddr: 0x{X:08}, size: 0x{X:08}\n", .{
-                region.file_offset,
-                region.start_address,
-                region.region_size,
-            });
-        }
-    }
-    context.regions = regions.items;
-    try opencsd.checkError(opencsd.dt_add_binfile_region_mem_acc(
-        dt.handle,
-        regions.items.ptr,
-        @intCast(regions.items.len),
-        opencsd.MEM_SPACE_ANY,
-        program_elf_path.ptr,
-    ));
-    defer {
-        if (regions.items.len > 0) {
-            _ = opencsd.dt_remove_mem_acc(
-                dt.handle,
-                regions.items[0].start_address,
-                opencsd.MEM_SPACE_ANY
-            );
-        }
-    }
-
-    var dump_options: ObjdumpOptions = .{
-        .target_query = .{
-            .cpu_arch = .thumb,
-            .cpu_model = &std.Target.arm.cpu.cortex_m33,
-            .cpu_features_add = std.Target.arm.featureSet(&.{
-                .fp_armv8d16sp,
-                .dsp,
-            }),
-        },
-    };
-    if (init.environ_map.get("OBJDUMP")) |dump_exe| {
-        dump_options.exe = dump_exe;
-    }
-
-    context.dump = try collectObjdump(program_elf_path, io, init.gpa, regions.items, dump_options);
-    defer context.dump.deinit(init.gpa);
+    try elf_context.openCapstoneHandle();
 
     var trace_file = try Io.Dir.openFile(.cwd(), io, trace_bin_path, .{
         .mode = .read_only,
